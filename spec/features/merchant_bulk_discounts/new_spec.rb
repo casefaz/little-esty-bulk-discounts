6495eee13e8c2cd89a2da6d4ca4e4ef988896fd8
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-09 19:20:49 -0600

    Testing and sad pathing for merchant bulk discount create'

--- spec/features/merchant_bulk_discounts/new_spec.rb
+++ spec/features/merchant_bulk_discounts/new_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'form for new discount', type: :feature do
   describe 'merchant bulk discount create' do 
     it 'has a form to add a new bulk discount' do 
       merch = create(:merchant)
-      
       visit new_merchant_bulk_discount_path(merch.id)
 
       expect(page).to have_content("New Form")
@@ -16,14 +15,25 @@ RSpec.describe 'form for new discount', type: :feature do
 
     it 'doesnt work if field is left blank' do 
       merch = create(:merchant)
-      
       visit new_merchant_bulk_discount_path(merch.id)
-
+    
       expect(page).to have_content("New Form")
       fill_in('Percentage', with: 42)
       click_on('Create Bulk discount')
       expect(current_path).to eq(new_merchant_bulk_discount_path(merch.id))
       expect(page).to have_content("Error: Missing Field")
+    end
+
+    it 'has new discount on index page' do 
+      merch = create(:merchant)
+      visit new_merchant_bulk_discount_path(merch.id)
+
+      expect(page).to have_content("New Form")
+      fill_in('Percentage', with: 42)
+      fill_in('Quantity Threshold', with: 5)
+      click_on('Create Bulk discount')
+      expect(current_path).to eq merchant_bulk_discounts_path(merch.id)
+      expect(page).to have_content('Discount: %42')
     end
   end
 end
