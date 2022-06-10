59ae7b0dfa2c433f999869a7d9fb5b552ef8f3a9
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-09 18:38:51 -0600

    Add new spec and feature for new form

--- spec/features/merchant_bulk_discounts/new_spec.rb
+++ spec/features/merchant_bulk_discounts/new_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'form for new discount', type: :feature do 
-  
+  describe 'merchant bulk discount create' do 
+    it 'has a form to add a new bulk discount' do 
+      merch = create(:merchant)
+      
+      visit new_merchant_bulk_discount_path(merch.id)
+      expect(page).to have_content("New Form")
+      fill_in('bulk_discount[percentage]', with: 42)
+      fill_in('bulk_discount[quantity_threshold]', with: 5)
+      click_on('Submit')
+      expect(current_path).to eq merchant_bulk_discounts_path(merch.id)
+    end
+  end
 end
