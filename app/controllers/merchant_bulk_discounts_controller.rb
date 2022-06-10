44e74aaa4a83a385a83487cc9e996d193b29e660
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-09 19:11:49 -0600

    Almost finished partial with error for sad path

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -9,6 +9,22 @@ class MerchantBulkDiscountsController < ApplicationController
   end
 
   def new
-    
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.new
   end
+
+  def create 
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.bulk_discounts.new(bulk_discount_params)
+    if discount.save
+      redirect_to merchant_bulk_discounts_path(merchant.id), notice: 'Success!'
+    else
+      redirect_to new_merchant_bulk_discount_path(merchant.id), notice: "Error: Missing Field"
+    end
+  end
+
+  private
+    def bulk_discount_params
+      params.require(:bulk_discount).permit(:percentage, :quantity_threshold)
+    end
 end
--- spec/features/merchant_bulk_discounts/new_spec.rb
+++ spec/features/merchant_bulk_discounts/new_spec.rb
@@ -6,11 +6,24 @@ RSpec.describe 'form for new discount', type: :feature do
       merch = create(:merchant)
       
       visit new_merchant_bulk_discount_path(merch.id)
+
       expect(page).to have_content("New Form")
-      fill_in('bulk_discount[percentage]', with: 42)
-      fill_in('bulk_discount[quantity_threshold]', with: 5)
-      click_on('Submit')
+      fill_in('Percentage', with: 42)
+      fill_in('Quantity Threshold', with: 5)
+      click_on('Create Bulk discount')
       expect(current_path).to eq merchant_bulk_discounts_path(merch.id)
+    end
+
+    it 'doesnt work if field is left blank' do 
+      merch = create(:merchant)
+      
+      visit new_merchant_bulk_discount_path(merch.id)
+
+      expect(page).to have_content("New Form")
+      fill_in('Percentage', with: 42)
+      click_on('Create Bulk discount')
+      expect(current_path).to eq(new_merchant_bulk_discount_path(merch.id))
+      expect(page).to have_content("Error: Missing Field")
     end
   end
 end
