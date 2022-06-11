17616be276283b0be195099f641a18fc0baddf78
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-11 16:44:06 -0600

    Add sad path for where link is not applicable

--- spec/features/merchant_invoices/show_spec.rb
+++ spec/features/merchant_invoices/show_spec.rb
@@ -204,7 +204,7 @@ RSpec.describe 'Merchant_Invoices Show Page', type: :feature do
   end
 
   describe 'Link to applied discounts' do 
-    it 'has a link next to each invoice item for the bulk discount that was applied (if any)' do 
+    it 'has a link next to each invoice item for the bulk discount that was applied' do 
       merchant = create(:merchant)
       items = create_list(:item, 2, merchant: merchant)
       invoices = create_list(:invoice, 3)
@@ -235,6 +235,23 @@ RSpec.describe 'Merchant_Invoices Show Page', type: :feature do
       expect(page).to have_content(bulk2.percentage)
       expect(page).to have_content(bulk2.quantity_threshold)
       expect(page).to_not have_content(bulk1.percentage)
+    end
+
+    it 'has no link if no discount was applied' do
+      merchant = create(:merchant)
+      items = create_list(:item, 2, merchant: merchant)
+      invoices = create_list(:invoice, 3)
+      invoice_item1 = create(:invoice_item, item: items[0], invoice: invoices[2], unit_price: 50000, quantity: 20)
+      invoice_item2 = create(:invoice_item, item: items[1], invoice: invoices[2], unit_price: 2500, quantity: 5)
+      invoice_item3 = create(:invoice_item, item: items[1], invoice: invoices[1], unit_price: 3000, quantity: 25)
+      bulk1 = create(:bulk_discount, merchant: merchant)
+      bulk2 = create(:bulk_discount, quantity_threshold: 25, percentage: 30.0, merchant: merchant)
+
+      visit merchant_invoice_path(merchant.id, invoices[2].id)
+
+      within "#invoiceItem-#{invoice_item2.id}" do 
+        expect(page).to have_content('no discount applied')
+      end
     end
   end
 end
